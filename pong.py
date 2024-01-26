fd3c76d81add9e715aeeaf79271712a717ae7071
sena-00 <156020094+sena-00@users.noreply.github.com>
2024-01-26 14:33:17 -0400

    updated pong paddles

--- pong.py
+++ pong.py
@@ -1,16 +1,51 @@
 import pygame
 pygame.init()
 
+#defines characteristics of the windows display
 WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
+FPS = 60
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
+
+#OOP for paddles
+class Paddle:
+    COLOR = WHITE
+
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+def draw(win, paddles):
+    win.fill(BLACK)
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    pygame.display.update()
 
 #loop to check if the application is running
 def main():
     run = True
+    #regulates the time clock var can run
+    clock = pygame.time.Clock()
+
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH,  HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    
 
     while run:
+        clock.tick(FPS)
+        draw(WIN, [left_paddle, right_paddle])
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
